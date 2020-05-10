5a14bf3136ccb3753dd9ee54af7ae49c940f2e6c
冯文议 <xfsy_2015@163.com>
2020-05-11 06:08:58 +0800

    Spring Boot Docker

---
+++ spring-boot-docker/src/main/java/com/fengwenyi/spring_boot/docker/DockerApplication.java
@@ -0,0 +1,17 @@
+package com.fengwenyi.spring_boot.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Erwin Feng
+ * @since 2020/5/11 5:55 上午
+ */
+@SpringBootApplication
+public class DockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerApplication.class, args);
+    }
+
+}
