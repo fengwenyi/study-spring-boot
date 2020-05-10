25612d7b19fd2bf33263296b19ed1499a0c0eaff
冯文议 <xfsy_2015@163.com>
2020-05-11 06:10:04 +0800

    提交

---
+++ spring-boot-security-sms/src/main/java/com/fengwenyi/spring_boot_security_sms/SpringBootSecuritySmsApplication.java
@@ -0,0 +1,13 @@
+package com.fengwenyi.spring_boot_security_sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecuritySmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecuritySmsApplication.class, args);
+	}
+
+}
---
+++ spring-boot-security-sms/src/test/java/com/fengwenyi/spring_boot_security_sms/SpringBootSecuritySmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fengwenyi.spring_boot_security_sms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecuritySmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
