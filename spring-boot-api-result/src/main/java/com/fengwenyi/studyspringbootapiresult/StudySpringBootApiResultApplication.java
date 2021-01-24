c4362a5ecd2e652c35fcbf3a627405d5f78003f0
Erwin <xfsy_2015@163.com>
2021-01-25 00:43:11 +0800

    Spring Boot整合api-result

---
+++ spring-boot-api-result/src/main/java/com/fengwenyi/studyspringbootapiresult/StudySpringBootApiResultApplication.java
@@ -0,0 +1,16 @@
+package com.fengwenyi.studyspringbootapiresult;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Erwin Feng
+ * @since 2021-01-25
+ */
+@SpringBootApplication
+public class StudySpringBootApiResultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySpringBootApiResultApplication.class, args);
+    }
+}
